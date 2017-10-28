4aa90b681a425ec889e0eb1e425613f2c6a5d7a8
linonetwo <linonetwo012@gmail.com>
2017-10-28 14:42:00 +0800

    Revert "refactor: try functionization module set up for supporing multithreading"
    This reverts commit 52a202d99d7d5994462e0f36078ea515010afb71.

--- src/module/ChsNameOptimizer.js
+++ src/module/ChsNameOptimizer.js
@@ -1,17 +1,4 @@
 // @flow
-import { FAMILY_NAME_1, FAMILY_NAME_2, SINGLE_NAME, DOUBLE_NAME_1, DOUBLE_NAME_2 } from './CHS_NAMES';
-
-/** 模块类型 */
-const type = 'optimizer';
-
-/**
- * 模块初始化
- *
- * @param {Segment} segment 分词接口
- */
-const init = function(segment) {
-  ChsNameOptimizer.segment = segment;
-};
 
 /**
  * 人名优化模块
@@ -19,9 +6,26 @@ const init = function(segment) {
  * @author 老雷<leizongmin@gmail.com>
  * @version 0.1
  */
-const ChsNameOptimizer = {
 
-}
+const FAMILY_NAME_1 = require('./CHS_NAMES').FAMILY_NAME_1;
+const FAMILY_NAME_2 = require('./CHS_NAMES').FAMILY_NAME_2;
+const SINGLE_NAME = require('./CHS_NAMES').SINGLE_NAME;
+const DOUBLE_NAME_1 = require('./CHS_NAMES').DOUBLE_NAME_1;
+const DOUBLE_NAME_2 = require('./CHS_NAMES').DOUBLE_NAME_2;
+
+const debug = console.log;
+
+/** 模块类型 */
+exports.type = 'optimizer';
+
+/**
+ * 模块初始化
+ *
+ * @param {Segment} segment 分词接口
+ */
+exports.init = function (segment) {
+  exports.segment = segment;
+};
 
 /**
  * 对可能是人名的单词进行优化
@@ -29,8 +33,9 @@ const ChsNameOptimizer = {
  * @param {array} words 单词数组
  * @return {array}
  */
-ChsNameOptimizer.doOptimize = function(words) {
-  const POSTAG = ChsNameOptimizer.segment.POSTAG;
+exports.doOptimize = function (words) {
+  // debug(words);
+  const POSTAG = exports.segment.POSTAG;
   let i = 0;
 
   /* 第一遍扫描 */
@@ -38,12 +43,10 @@ ChsNameOptimizer.doOptimize = function(words) {
     var word = words[i];
     var nextword = words[i + 1];
     if (nextword) {
+      // debug(nextword);
       // 如果为  "小|老" + 姓
-      if (
-        nextword &&
-        (word.w == '小' || word.w == '老') &&
-        (nextword.w in FAMILY_NAME_1 || nextword.w in FAMILY_NAME_2)
-      ) {
+      if (nextword && (word.w == '小' || word.w == '老') &&
+      (nextword.w in FAMILY_NAME_1 || nextword.w in FAMILY_NAME_2)) {
         words.splice(i, 2, {
           w: word.w + nextword.w,
           p: POSTAG.A_NR,
@@ -53,10 +56,8 @@ ChsNameOptimizer.doOptimize = function(words) {
       }
 
       // 如果是 姓 + 名（2字以内）
-      if (
-        (word.w in FAMILY_NAME_1 || word.w in FAMILY_NAME_2) &&
-        ((nextword.p & POSTAG.A_NR) > 0 && nextword.w.length <= 2)
-      ) {
+      if ((word.w in FAMILY_NAME_1 || word.w in FAMILY_NAME_2) &&
+      ((nextword.p & POSTAG.A_NR) > 0 && nextword.w.length <= 2)) {
         words.splice(i, 2, {
           w: word.w + nextword.w,
           p: POSTAG.A_NR,
@@ -67,17 +68,16 @@ ChsNameOptimizer.doOptimize = function(words) {
 
       // 如果相邻两个均为单字且至少有一个字是未识别的，则尝试判断其是否为人名
       if (!word.p || !nextword.p) {
-        if (
-          (word.w in SINGLE_NAME && word.w == nextword.w) ||
-          (word.w in DOUBLE_NAME_1 && nextword.w in DOUBLE_NAME_2)
-        ) {
+        if ((word.w in SINGLE_NAME && word.w == nextword.w) ||
+        (word.w in DOUBLE_NAME_1 && nextword.w in DOUBLE_NAME_2)) {
           words.splice(i, 2, {
             w: word.w + nextword.w,
             p: POSTAG.A_NR,
           });
           // 如果上一个单词可能是一个姓，则合并
           const preword = words[i - 1];
-          if (preword && (preword.w in FAMILY_NAME_1 || preword.w in FAMILY_NAME_2)) {
+          if (preword &&
+          (preword.w in FAMILY_NAME_1 || preword.w in FAMILY_NAME_2)) {
             words.splice(i - 1, 2, {
               w: preword.w + word.w + nextword.w,
               p: POSTAG.A_NR,
@@ -90,7 +90,9 @@ ChsNameOptimizer.doOptimize = function(words) {
       }
 
       // 如果为 无歧义的姓 + 名（2字以内） 且其中一个未未识别词
-      if ((word.w in FAMILY_NAME_1 || word.w in FAMILY_NAME_2) && (!word.p || !nextword.p)) {
+      if ((word.w in FAMILY_NAME_1 || word.w in FAMILY_NAME_2) &&
+          (!word.p || !nextword.p)) {
+        // debug(word, nextword);
         words.splice(i, 2, {
           w: word.w + nextword.w,
           p: POSTAG.A_NR,
@@ -109,7 +111,8 @@ ChsNameOptimizer.doOptimize = function(words) {
     var nextword = words[i + 1];
     if (nextword) {
       // 如果为 姓 + 单字名
-      if ((word.w in FAMILY_NAME_1 || word.w in FAMILY_NAME_2) && nextword.w in SINGLE_NAME) {
+      if ((word.w in FAMILY_NAME_1 || word.w in FAMILY_NAME_2) &&
+      nextword.w in SINGLE_NAME) {
         words.splice(i, 2, {
           w: word.w + nextword.w,
           p: POSTAG.A_NR,
@@ -125,6 +128,3 @@ ChsNameOptimizer.doOptimize = function(words) {
 
   return words;
 };
-
-export { type, init };
-export default ChsNameOptimizer;
