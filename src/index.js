12b76d01c9062455c2c659cdd535e21643a054ce
lindongwu <lindongwu@memect.co>
2017-10-28 12:16:10 +0800

    refactor: export all components

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// @flow
+export default './Segment';
+
+export { Segment } from './Segment';
+export { POSTAG } from './POSTAG';
+export { pangu, panguExtend1, panguExtend2, names, wildcard, synonym, stopword } from './knowledge';
+export {
+  CHS_NAMES,
+  ChsNameTokenizer,
+  DictOptimizer,
+  EmailOptimizer,
+  PunctuationTokenizer,
+  URLTokenizer,
+  ChsNameOptimizer,
+  DatetimeOptimizer,
+  DictTokenizer,
+  ForeignTokenizer,
+  SingleTokenizer,
+  WildcardTokenizer,
+} from './module';
---
+++ src/module/index.js
@@ -0,0 +1,13 @@
+// @flow
+export CHS_NAMES from './CHS_NAMES';
+export ChsNameTokenizer from './ChsNameTokenizer';
+export DictOptimizer from './DictOptimizer';
+export EmailOptimizer from './EmailOptimizer';
+export PunctuationTokenizer from './PunctuationTokenizer';
+export URLTokenizer from './URLTokenizer';
+export ChsNameOptimizer from './ChsNameOptimizer';
+export DatetimeOptimizer from './DatetimeOptimizer';
+export DictTokenizer from './DictTokenizer';
+export ForeignTokenizer from './ForeignTokenizer';
+export SingleTokenizer from './SingleTokenizer';
+export WildcardTokenizer from './WildcardTokenizer';
