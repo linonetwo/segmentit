d7668f70ecd35aa84370e01b001e623b2275a812
linonetwo <linonetwo012@gmail.com>
2017-11-02 19:03:37 +0800

    test: English version POS tagging test

---
+++ __tests__/POSTAG.test.js
@@ -0,0 +1,32 @@
+/** @flow */
+
+import assert from 'assert';
+import Segment, { useDefault, chinesePOSTag, englishPOSTag } from '../src';
+
+describe('词类测试', () => {
+  let segment = null;
+
+  it('init', () => {
+    segment = useDefault(new Segment());
+  });
+
+  it('中文词类标注', () => {
+    const toPOS = arr => arr.map(w => chinesePOSTag(w.p));
+    const equal = (a, b) => {
+      const aString = toPOS(segment.doSegment(a));
+      assert.equal(aString.toString('\t'), b.toString('\t'));
+    };
+
+    equal('因为李三买了一张三角桌子', ['连词 连语素', '人名', '动词 动语素', '助词 助语素', '数量词', '数词 数语素', '名词 名语素']);
+  });
+
+  it('结巴风格词类标注', () => {
+    const toPOS = arr => arr.map(w => englishPOSTag(w.p));
+    const equal = (a, b) => {
+      const aString = toPOS(segment.doSegment(a));
+      assert.equal(aString.toString('\t'), b.toString('\t'));
+    };
+
+    equal('陈晨和林迪是好朋友', ['nr', 'c', 'nr', 'v', 'a', 'n']);
+  });
+});
