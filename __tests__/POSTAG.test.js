92162cd6f3703c53099e39eb611e87679f07e728
linonetwo <linonetwo012@gmail.com>
2017-11-07 21:01:32 +0800

    refactor: make test description more presice

--- __tests__/POSTAG.test.js
+++ __tests__/POSTAG.test.js
@@ -3,16 +3,14 @@
 import assert from 'assert';
 import Segmentit, { useDefault, cnPOSTag, enPOSTag } from '../src';
 
-describe('词类测试', () => {
-  let segmentit = null;
-
+let segmentit = null;
+let toPOS = null;
+let equal = null;
+describe('给出词类的中文解释', () => {
   it('init', () => {
     segmentit = useDefault(new Segmentit());
-  });
-
-  it('中文词类标注', () => {
-    const toPOS = arr => arr.map(w => cnPOSTag(w.p));
-    const equal = (a, b) => {
+    toPOS = arr => arr.map(w => cnPOSTag(w.p));
+    equal = (a, b) => {
       const aString = toPOS(segmentit.doSegment(a));
       assert.equal(
         aString.toString('\t'),
@@ -20,13 +18,18 @@ describe('词类测试', () => {
         `${a} should be ${JSON.stringify(b)} but is ${JSON.stringify(aString)}`,
       );
     };
+  });
 
+  it('给出词类的中文解释', () => {
     equal('因为李三买了一张三角桌子', ['连词 连语素', '人名', '动词 动语素', '助词 助语素', '数量词', '数词 数语素', '名词 名语素']);
   });
+});
 
-  it('结巴风格词类标注', () => {
-    const toPOS = arr => arr.map(w => enPOSTag(w.p));
-    const equal = (a, b) => {
+describe('给出词类的中文解释', () => {
+  it('init', () => {
+    segmentit = useDefault(new Segmentit());
+    toPOS = arr => arr.map(w => enPOSTag(w.p));
+    equal = (a, b) => {
       const aString = toPOS(segmentit.doSegment(a));
       assert.equal(
         aString.toString('\t'),
@@ -34,7 +37,8 @@ describe('词类测试', () => {
         `${a} should be ${JSON.stringify(b)} but is ${JSON.stringify(aString)}`,
       );
     };
-
+  });
+  it('给出结巴风格词类标注', () => {
     equal('陈晨和林迪是好朋友', ['nr', 'c', 'nr', 'v', 'a', 'n']);
 
     equal('一条红色内裤', ['mq', 'a', 'n']);
