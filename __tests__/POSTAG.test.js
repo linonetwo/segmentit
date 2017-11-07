7e32decc70e6b043b277aad20beeee4a7f22ea6b
linonetwo <linonetwo012@gmail.com>
2017-11-07 11:49:35 +0800

    test: add POS test for colors

--- __tests__/POSTAG.test.js
+++ __tests__/POSTAG.test.js
@@ -14,7 +14,11 @@ describe('词类测试', () => {
     const toPOS = arr => arr.map(w => cnPOSTag(w.p));
     const equal = (a, b) => {
       const aString = toPOS(segmentit.doSegment(a));
-      assert.equal(aString.toString('\t'), b.toString('\t'));
+      assert.equal(
+        aString.toString('\t'),
+        b.toString('\t'),
+        `${a} should be ${JSON.stringify(b)} but is ${JSON.stringify(aString)}`,
+      );
     };
 
     equal('因为李三买了一张三角桌子', ['连词 连语素', '人名', '动词 动语素', '助词 助语素', '数量词', '数词 数语素', '名词 名语素']);
@@ -24,9 +28,19 @@ describe('词类测试', () => {
     const toPOS = arr => arr.map(w => enPOSTag(w.p));
     const equal = (a, b) => {
       const aString = toPOS(segmentit.doSegment(a));
-      assert.equal(aString.toString('\t'), b.toString('\t'));
+      assert.equal(
+        aString.toString('\t'),
+        b.toString('\t'),
+        `${a} should be ${JSON.stringify(b)} but is ${JSON.stringify(aString)}`,
+      );
     };
 
     equal('陈晨和林迪是好朋友', ['nr', 'c', 'nr', 'v', 'a', 'n']);
+
+    equal('一条红色内裤', ['mq', 'a', 'n']);
+    equal('一条碧绿的内裤', ['mq', 'a', 'u', 'n']);
+    equal('你头顶碧绿的草原', ['r', 'n', 'a', 'u', 'n']);
+
+    equal('一只政府的走狗', ['m', 'd', 'n', 'u', 'n']);
   });
 });
