430d2c06517bbb0c2eef2339360fd3761ddeb7ff
linonetwo <linonetwo012@gmail.com>
2019-02-08 13:05:13 +0800

    fix: properly config preval

--- src/knowledge/index.js
+++ src/knowledge/index.js
@@ -1,24 +1,25 @@
 // @flow
-import preval from 'preval.macro'
+import preval from 'preval.macro';
 
-const imports = `
-  const { readFileSync } = require('fs');
-  const { join } = require('path');
-`;
+const importExport = `
+  const fs = require('fs');
+  const path = require('path');
+  module.exports = fs.readFileSync(path.join(__dirname, '`;
+const tail = ".txt'), 'utf8')";
 
 // 盘古词典
-export const pangu: string = preval`${imports} readFileSync(join(__dirname, 'pangu.txt'), 'utf8')`;
+export const pangu: string = preval`${importExport}pangu${tail}`;
 // 扩展词典（用于调整原盘古词典）
-export const panguExtend1: string = preval`${imports} readFileSync(join(__dirname, 'panguExtend1.txt'), 'utf8')`;
-export const panguExtend2: string = preval`${imports} readFileSync(join(__dirname, 'panguExtend2.txt'), 'utf8')`;
+export const panguExtend1: string = preval`${importExport}panguExtend1${tail}`;
+export const panguExtend2: string = preval`${importExport}panguExtend2${tail}`;
 // 常见名词、人名
-export const names: string = preval`${imports} readFileSync(join(__dirname, 'names.txt'), 'utf8')`;
+export const names: string = preval`${importExport}names${tail}`;
 // 通配符
-export const wildcard: string = preval`${imports} readFileSync(join(__dirname, 'wildcard.txt'), 'utf8')`;
+export const wildcard: string = preval`${importExport}wildcard${tail}`;
 // 同义词
-export const synonym: string = preval`${imports} readFileSync(join(__dirname, 'synonym.txt'), 'utf8')`;
+export const synonym: string = preval`${importExport}synonym${tail}`;
 // 停止符
-export const stopword: string = preval`${imports} readFileSync(join(__dirname, 'stopword.txt'), 'utf8')`;
+export const stopword: string = preval`${importExport}stopword${tail}`;
 
 // 字典集，方便 import
 export const dicts: string[] = [pangu, panguExtend1, panguExtend2, names, wildcard];
